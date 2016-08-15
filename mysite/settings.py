7b6eb98b7d85e7cd9e937fc2b034c846330bde87
takechanman1228 <sigur.ku@gmail.com>
2016-08-15 23:17:35 +0900

    django rest framework pagenation

--- mysite/settings.py
+++ mysite/settings.py
@@ -101,6 +101,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 2
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
